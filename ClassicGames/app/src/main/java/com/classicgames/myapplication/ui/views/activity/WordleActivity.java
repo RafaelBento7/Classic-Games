b94b05b9b756d0d2e61148c6fb3fdfc0b1aa7708
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-06-30 16:19:10 +0100

    Wordle Layout

---
+++ ClassicGames/app/src/main/java/com/classicgames/myapplication/ui/views/activity/WordleActivity.java
@@ -0,0 +1,16 @@
+package com.classicgames.myapplication.ui.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.classicgames.myapplication.R;
+
+public class WordleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wordle);
+    }
+}
