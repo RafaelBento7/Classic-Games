36c2bed22bd30c9440860c4aa1c4c7baf845966b
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-07-12 18:00:02 +0100

    Wordle added to Records page

--- ClassicGames/app/src/main/java/com/classicgames/myapplication/ui/views/fragment/LeaderboardFragment.java
+++ ClassicGames/app/src/main/java/com/classicgames/myapplication/ui/views/fragment/LeaderboardFragment.java
@@ -20,30 +20,40 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentLeaderboardBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-
+        // Snake
         binding.LeaderboardTvSnakeBigMapRecord.setText(String.valueOf(MyApplication.getInstance().getRecords().getSnakeBigRecord()));
         binding.LeaderboardTvSnakeMediumMapRecord.setText(String.valueOf(MyApplication.getInstance().getRecords().getSnakeMediumRecord()));
         binding.LeaderboardTvSnakeSmallMapRecord.setText(String.valueOf(MyApplication.getInstance().getRecords().getSnakeSmallRecord()));
+        // TrueColors
         binding.LeaderboardTvTrueColorsRecord.setText(String.valueOf(MyApplication.getInstance().getRecords().getTrueColorsRecord()));
+        // Mastermind
         int[] mastermindRecords = MyApplication.getInstance().getRecords().getMastermindRecord();
-        String minutes = String.valueOf(mastermindRecords[0]);
-        String seconds = String.valueOf(mastermindRecords[1]);
-        String hours = "";
-        int hoursInt = 0;
-        if (mastermindRecords[1] < 10) seconds = "0" + seconds;
-        if (mastermindRecords[0] > 59) {
-            hoursInt = mastermindRecords[0] / 60;
-            mastermindRecords[0] = mastermindRecords[0] % 60;
-            minutes = String.valueOf(mastermindRecords[0]);
-            hours = String.valueOf(hoursInt);
-        }
-        if (mastermindRecords[0] < 10) minutes = "0" + minutes;
-        if (hoursInt < 10) hours = "0" + hours;
-        if (Integer.parseInt(hours) == 0)
-            binding.LeaderboardTvMastermindTimeRecord.setText(getResources().getString(R.string.timer_minutes_seconds,minutes,seconds));
-        else binding.LeaderboardTvMastermindTimeRecord.setText(getResources().getString(R.string.timer_hour_minutes_seconds,hours,minutes,seconds));
+        binding.LeaderboardTvMastermindTimeRecord.setText(GetTimerRecordStr(mastermindRecords));
         binding.LeaderboardTvMastermindAttemptsRecord.setText(String.valueOf(mastermindRecords[2]));
+        // Wordle
+        int[] wordleRecords = MyApplication.getInstance().getRecords().getWordleRecord();
+        binding.LeaderboardTvWordlePointsRecord.setText(String.valueOf(wordleRecords[2]));
+        binding.LeaderboardTvWordleTimeRecord.setText(GetTimerRecordStr(mastermindRecords));
 
         return view;
+    }
+
+    private String GetTimerRecordStr(int[] records) {
+        String minutes = String.valueOf(records[0]);
+        String seconds = String.valueOf(records[1]);
+        String hours = "";
+        int hoursInt = 0;
+        if (records[1] < 10) seconds = "0" + seconds;
+        if (records[0] > 59) {
+            hoursInt = records[0] / 60;
+            records[0] = records[0] % 60;
+            minutes = String.valueOf(records[0]);
+            hours = String.valueOf(hoursInt);
+        }
+        if (records[0] < 10) minutes = "0" + minutes;
+        if (hoursInt < 10) hours = "0" + hours;
+        if (Integer.parseInt(hours) == 0)
+            return getResources().getString(R.string.timer_minutes_seconds,minutes,seconds);
+        else return getResources().getString(R.string.timer_hour_minutes_seconds,hours,minutes,seconds);
     }
 }
